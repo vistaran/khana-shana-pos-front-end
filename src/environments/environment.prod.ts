6038bfb92696d62668ccd1cfc6bb8a91fe2400e7
Vandanaba Gohil <vandanaba.gohil17@gmail.com>
2023-03-15 17:36:55 +0530

    - api url change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.' + location.hostname + '/api/'
+    apiUrl: 'https://api.' + location.hostname.split('.')[0] + '/api/'
 };
